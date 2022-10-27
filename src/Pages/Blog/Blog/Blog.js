1a8c095e4684c2023eea00848c8ea4db32501aa9
Kafi09 <kaficsmy9@gmail.com>
2022-10-27 16:37:08 +0600

    add blog q1

--- src/Pages/Blog/Blog/Blog.js
+++ src/Pages/Blog/Blog/Blog.js
@@ -3,7 +3,21 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h2>this is a blog</h2>
+            <section class="px-5 pt-5">
+                <h1 class="fs-4">Q1:What is CORS?</h1>
+                <p class="fs-5">Ans: Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources.Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources.</p>
+
+                <h1 class="fs-4">Q2:</h1>
+                <p class="fs-5">Ans: </p>
+
+                <h1 class="fs-4">Q3:</h1>
+                <p class="fs-5">Ans:</p>
+
+                <h1 class="fs-4">Q4:</h1>
+                <p class="fs-5">Ans:</p>
+                
+
+            </section>
         </div>
     );
 };
