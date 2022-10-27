3319237767bf05f79b3a0d2401476981a7386551
Kafi09 <kaficsmy9@gmail.com>
2022-10-27 16:46:54 +0600

    add q2

--- src/Pages/Blog/Blog/Blog.js
+++ src/Pages/Blog/Blog/Blog.js
@@ -7,8 +7,14 @@ const Blog = () => {
                 <h1 class="fs-4">Q1:What is CORS?</h1>
                 <p class="fs-5">Ans: Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources.Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources.</p>
 
-                <h1 class="fs-4">Q2:</h1>
-                <p class="fs-5">Ans: </p>
+                <h1 class="fs-4">Q2:Why are you using firebase? What other options do you have to implement authentication?</h1>
+                <p class="fs-5">Ans: The Firebase Realtime Database lets you build rich, collaborative applications by allowing secure access to the database directly from client-side code. Data is persisted locally, and even while offline, realtime events continue to fire, giving the end user a responsive experience.
+                    
+                    other alternate options for authentication:
+                    - Auth0
+                    - Okta
+                    - Keycloak etc.
+                </p>
 
                 <h1 class="fs-4">Q3:</h1>
                 <p class="fs-5">Ans:</p>
